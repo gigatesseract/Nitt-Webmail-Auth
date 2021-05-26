87cd31adc4c9e6d76c5a3705d5d200eed2a4f2a5
N Praveen Chandhar <neevarp.star@gmail.com>
2021-05-26 18:14:32 +0530

    remove log messages

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 import Authenticator from "./utils/Authenticator.js";
 
-(async() =>{
-    const authenticator = new Authenticator({ useWebmailDirectly: true });
-const response = await authenticator.login({
-  username: "<>",
-  password: "<>",
-  pingBeforeLogin: true,
-});
-console.log(response);
-})();
+
+export default Authenticator
